5588fae84953cede3b7b5a475cf4b86d288042cd
Israt jahan <140290748+Israt-1234@users.noreply.github.com>
2023-08-12 02:00:04 +0600

    Add files via upload
    Codeforces Round 849 (Div. 4)

---
+++ B. Following Directions.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--){
+        int t,x=0,y=0,ans=0;
+        cin>>t;
+        string s;
+        cin>>s;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='U')
+               y++;
+            else if(s[i]=='R')
+                x++;
+            else if(s[i]=='D')
+                y--;
+            else if(s[i]=='L')
+                x--;
+                if(x==1 && y==1){
+                ans=1;
+                 break;
+                }
+        }
+
+       if(ans==1)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+    return 0;
+}
