2f5f4649f71c06046af1910e37a4da67176eb576
Israt jahan <140290748+Israt-1234@users.noreply.github.com>
2023-11-18 09:45:28 +0600

    Add files via upload

---
+++ A. Game with Integers.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes cout<<"Yes"<<endl
+#define No  cout<<"No"<<endl
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s;
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        if(n%3==0) cout<<"Second"<<endl;
+        else cout<<"First"<<endl;
+
+    }
+    return 0;
+}
+
