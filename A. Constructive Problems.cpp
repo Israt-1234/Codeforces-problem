25f0b9baf8e7d13b2f79634fd86c98fb50cca826
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2023-12-17 22:37:38 +0600

    Add files via upload

---
+++ A. Constructive Problems.cpp
@@ -0,0 +1,19 @@
+/*........Hard work and consistency is the only way to success........ */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=1005;
+//int d[N][N],pref[N][N];
+int main()
+{
+    // clock_t st= clock();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n; cin>>n;
+    while(n--){
+        int k,l;
+        cin>>k>>l;
+        cout<<max(k,l)<<endl;
+    }
+    return 0;
+}
