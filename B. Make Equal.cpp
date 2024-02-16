408afe097f7d3eae47569091052a0052d9e65f10
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-02-16 22:08:51 +0600

    Add files via upload

---
+++ B. Make Equal.cpp
@@ -0,0 +1,31 @@
+/*........Hard work and consistency is the only way to success........ */
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N = 1005;
+// int d[N][N],pref[N][N];
+int main()
+{
+    // clock_t st= clock();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int s[n];
+        int sum = 0, k = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> s[i];
+            sum += s[i];
+        }
+     k=sum/n;
+     for(int i=0;i<n;i++){
+        
+     }
+    }
+    return 0;
+}
