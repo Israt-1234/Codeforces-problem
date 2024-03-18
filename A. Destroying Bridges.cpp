f6ffda3523fc61cb9fb09108b01e75c00b9e4d84
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-03-18 22:06:49 +0600

    Add files via upload

---
+++ A. Destroying Bridges.cpp
@@ -0,0 +1,23 @@
+/*........Hard work and consistency is the only way to success........ */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=1005;
+//int d[N][N],pref[N][N];
+int main()
+{
+    // clock_t st= clock();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+      
+         if(k>=n-1)  
+         cout<<1<<endl;
+         else
+         cout<<n<<endl;
+    }
+    return 0;
+}
